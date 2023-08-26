1eae4bd59eb1fe89637568c27463ab2cb1e57340
dctime <daniel.chu916@gmail.com>
2023-08-26 16:13:35 +0800

    Feature: Full adder quartus real test worked

--- hack.v
+++ hack.v
@@ -4,6 +4,13 @@ module hack(
 	input a, b, c,
 	output sum , carry
 );
-	FullAdder fullAdder (.a(a), .b(b), .c(c), .sum(sum), .carry(carry));
+	wire nota, notb, notc, notsum, notcarry;
+	assign nota = ~a;
+	assign notb = ~b;
+	assign notc = ~c;
+	assign sum = ~notsum;
+	assign carry = ~notcarry;
+	
+	FullAdder fullAdder (.a(nota), .b(notb), .c(notc), .sum(notsum), .carry(notcarry));
 	
 endmodule
